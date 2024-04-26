ae10fe4f1f6761c301aa05a1d8037bd41786b634
EmmanuelIdeho <56139812+EmmanuelIdeho@users.noreply.github.com>
2024-04-26 09:37:37 -0400

    Create Secret Instructions.cs

---
+++ SecretInstructions.cs
@@ -0,0 +1,18 @@
+//My solution for CCC 2021-J3
+using System;
+
+public class Testing
+{
+    public static void Main(string[] args)
+    {
+      bool running = true;
+      String inputs[100];
+      int i;
+      Console.WriteLine("Write lines of encoded instructions\n");
+      while(running){
+        inputs[i] = Console.ReadLine();
+        i++;
+      }
+
+    }
+}
